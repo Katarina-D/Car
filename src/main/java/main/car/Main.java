eaba6a29f14b1b66d5421ec101f702278c276636
Katarina-D <Katarina@Katarinas-MacBook-Pro.local>
2021-03-14 16:22:40 +0100

    add travel method and required attributed for that

--- src/main/java/main/car/Main.java
+++ src/main/java/main/car/Main.java
@@ -7,6 +7,7 @@ public class Main {
         int b = 6;
         
         Car audi = new Car();
+        audi.printAttributes();
         audi.brand = "Audi";
         audi.model = "A4";
         audi.color = "black";
@@ -20,7 +21,9 @@ public class Main {
         Car fiat = new Car(2010);
         System.out.println("Godina proizvodnje " + fiat.getBuildYear());
         
-        Car lada = new Car("Lada", "Niva", "green", 2018);
+        Car lada = new Car("Lada", "Niva", "green", 2018, 0, 20, 5);
+        lada.printAttributes();
+        lada.travel(100);
         lada.printAttributes();
     }
     
